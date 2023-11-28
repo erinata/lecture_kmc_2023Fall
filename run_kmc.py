13dedbc6b26835a02ee43e759ff6b4ebe41c0cfd
Chungsang Tom Lam <erinata@gmail.com>
2023-11-28 11:56:43 -0500

    kmedoids

--- run_kmc.py
+++ run_kmc.py
@@ -2,6 +2,9 @@ import pandas
 import matplotlib.pyplot as pyplot
 from sklearn.cluster import KMeans
 
+from sklearn_extra.cluster import KMedoids
+
+
 from sklearn.metrics import silhouette_score 
 
 dataset = pandas.read_csv("dataset.csv")
@@ -29,11 +32,32 @@ def run_kmeans(n, dataset):
 
 silhouette_score_list = [run_kmeans(i, dataset) for i in [2,3,4,5,6,7,8]]
 
-print(silhouette_score_list)
-
 pyplot.scatter([2,3,4,5,6,7,8], silhouette_score_list)
 pyplot.savefig("silhouette_score_kmeans.png")
 pyplot.close()
+
+
+
+
+
+
+def run_kmedoids(n, dataset):
+  machine = KMedoids(n_clusters=n)
+  machine.fit(dataset)
+  results = machine.predict(dataset)
+  centroids = machine.cluster_centers_
+  pyplot.scatter(dataset[:,0],dataset[:,1], c=results)
+  pyplot.scatter(centroids[:,0], centroids[:, 1], c="red", marker="*", s=300)
+  pyplot.savefig("scatterplot_kmedoids_" + str(n) + ".png")
+  pyplot.close()
+  return silhouette_score(dataset, results, metric="euclidean")
+
+silhouette_score_list = [run_kmedoids(i, dataset) for i in [2,3,4,5,6,7,8]]
+
+pyplot.scatter([2,3,4,5,6,7,8], silhouette_score_list)
+pyplot.savefig("silhouette_score_kmedoids.png")
+pyplot.close()
+
 
 
 
